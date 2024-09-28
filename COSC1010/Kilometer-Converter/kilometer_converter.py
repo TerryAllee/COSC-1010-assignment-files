67989d44854ad210fa1320e803cdb0b217c953a8
Terry Allee <terryallee@westernwyoming.edu>
2024-09-28 13:09:21 -0600

    Add files via upload

--- COSC1010/Kilometer-Converter/kilometer_converter.py
+++ COSC1010/Kilometer-Converter/kilometer_converter.py
@@ -1,7 +1,29 @@
 #
-# Name
-# Date
-# Kilometer Converter Programming Project
-# COSC 2409 DNT
-#
-# Use comments liberally throughout the program. 
+# Name: Terry Allee
+# Date: September 28, 2024
+# Kilometer Conversion Programming Project
+# COSC 1010
+
+# Use comments liberally throughout the program.
+ 
+# This program converts kilometers to miles
+CONVERSION_FACTOR = 0.6214
+
+# The main function gets a distance in kilometers and calls the show_miles function to convert it. 
+def main ():
+    # Get the distance in kilometers 
+    kilometers = float (input('Enter a distance in kilometers:'))
+
+    # Display the distance converted to miles.
+    show_miles(kilometers)
+
+# The show_miles function converts the parameter km from kilometers to miles and displays the result.
+def show_miles (km):
+    # Calculate the miles.
+    miles = km * CONVERSION_FACTOR
+
+    # Display the miles.
+    print (km, 'kilometers equals', miles, 'miles')
+
+# Call the main function.
+main()
