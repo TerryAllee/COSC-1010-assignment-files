1ad4a7c8f25249d496d58e2afeb9f354bcba6665
Terry Allee <terryallee@westernwyoming.edu>
2024-09-28 13:20:23 -0600

    Update main.py

--- COSC1010/Feet-to-Inches/main.py
+++ COSC1010/Feet-to-Inches/main.py
@@ -1,7 +1,29 @@
 #
-# Name
-# Date
+# Name: Terry Allee
+# Date: September 28th, 2024
 # Feet to Inches Programming Project
-# COSC 2409 DNT
+# COSC 1010
 #
 # Use comments liberally throughout the program.
+
+
+# Constant for inches per foot
+INCHES_PER_FOOT = 12
+
+def feet_to_inches(feet):
+    #Constant will convert feet to inches
+    return feet * INCHES_PER_FOOT
+
+# Main function
+def main():
+    # Declare a variable to store that asks for user input. 
+    feet = float(input('Enter the number of feet: '))
+
+    # Declare a variable that stores the result of the calculation.
+    inches = feet_to_inches(feet)
+
+    # Display the result
+    print(f"{feet} foot or feet is equal to {inches} inches.")
+
+# Call the main function.
+main()
